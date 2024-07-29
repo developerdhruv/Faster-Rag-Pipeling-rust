0eacbb48e54d9c0e1ff980f17e672a4d1b560e5e
Dhruv Kumar <109203015+developerdhruv@users.noreply.github.com>
2024-07-30 00:31:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,34 +47,5 @@ print(f"Execution time: {end_time - start_time} seconds")
 
 #ollama llama 3
 
-full_response = []
-prompt = """
-You are a bot that makes recommendations for activities. You answer in very short sentences and do not include extra information.
-This is the recommended activity: {relevant_document}
-The user input is: {user_input}
-Compile a recommendation to the user based on the recommended activity and the user input.
-"""
-
-url = 'http://localhost:11434/api/generate'
-data = {
-    "model": "llama3",
-    "prompt": prompt.format(user_input=user_input, relevant_document=retrvant_document)
-}
-headers = {'Content-Type': 'application/json'}
-
-response = requests.post(url, data=json.dumps(data), headers=headers, stream=True)
-
-try:
-    for line in response.iter_lines():
-        # filter out keep-alive new lines
-        if line:
-            decoded_line = json.loads(line.decode('utf-8'))
-            # print(decoded_line['response'])  # uncomment to results, token by token
-            full_response.append(decoded_line['response'])
-finally:
-    response.close()
-    
-    
-print(''.join(full_response))
 
 
